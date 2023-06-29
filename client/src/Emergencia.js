66bd1da56eb25b3f66daa01a282d73ff42b56fc9
Eduardo Siegert Antunes <eantunes5@hotmail.com>
2023-06-28 21:01:13 -0300

    Commit emergencia

--- client/src/Emergencia.js
+++ client/src/Emergencia.js
@@ -1,56 +1,32 @@
-import './App.css';
-import React from "react"
-import SmallHeader from './components/small_header';
-import SelectButtons from './components/select_page';
-import PontosTitle from './components/pontos_title';
-import CardEmergencia from './components/cards_emergencia';
-//import Routes from "./Routes";
+import React, { useEffect, useState } from 'react';
 
 function Emergencia() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  function getUsers() {
+    const url = process.env.REACT_APP_API_URL;
+    fetch(`${url}/emergency`).then((result) => {
+      result.json().then((resp) => {
+        setUsers(resp);
+      });
+    });
+  }
 
   return (
     <div>
-      <SmallHeader/>
-      <SelectButtons page='emergencia'/>
-      <div className='div_container_cards_pontos'>
-        <br></br>
-        <PontosTitle text='TELEFONES ÚTEIS'/>
-        <div className='div_container_cards_emergencia'>
-          
-          <CardEmergencia 
-            nome='SAMU' 
-            numero='192' 
-            url_img='https://www.true.com.br/wp-content/uploads/2022/05/Ativo-5-300x300.png'
-            />
-          <CardEmergencia 
-            nome='Corpo de bombeiros' 
-            numero='193' 
-            url_img='https://upload.wikimedia.org/wikipedia/commons/5/5e/Brasao_bombeiros_parana_decada_90.png'
-            />
-          <CardEmergencia 
-            nome='Polícia militar' 
-            numero='190' 
-            url_img='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Logo_PMPR_2.svg/1200px-Logo_PMPR_2.svg.png'
-            />
-          <CardEmergencia 
-            nome='polícia civil' 
-            numero='197' 
-            url_img='https://upload.wikimedia.org/wikipedia/commons/2/2d/Logo_policia_civil.gif'
-            />
-          <CardEmergencia 
-            nome='polícia rodovíaria' 
-            numero='191' 
-            url_img='https://logodownload.org/wp-content/uploads/2016/10/policia-rodoviaria-logo-1-1.png'
-            />
-            <CardEmergencia 
-            nome='Defesa cívil' 
-            numero='199' 
-            url_img='https://cdn-icons-png.flaticon.com/512/126/126341.png'
-            />
+      {users.map((user) => (
+        <div key={user._id}>
+          <h2>{user.nome}</h2>
+          <img src={user.logo} alt={user.nome} />
+          <p>{user.numero}</p>
         </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Emergencia
+export default Emergencia;
