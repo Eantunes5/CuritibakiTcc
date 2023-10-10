eb5cd2cd845724e7a20acddab5da00f982ccb00a
Matheus Biscalchim <matheusbiscalchimmm@gmail.com>
2023-10-10 03:35:10 -0300

    Gets

--- server/src/routes/locals.route.js
+++ server/src/routes/locals.route.js
@@ -7,6 +7,8 @@ const router = Router();
 router.post('/', localsController.create);
 router.get('/', localsController.findAll);
 router.get('/:id',validId,validLocal, localsController.findById);
+router.get('/nome/:nome', localsController.findAllByName);
+router.get('/tipo/:tipo', localsController.findAllByType);
 router.patch('/:id',validId,validLocal, localsController.update);
 router.delete('/:id',validId, localsController.deleteById);
 
